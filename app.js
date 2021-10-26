adbe2b153251f26bc211fbe5204f3428540f3587
Roman Maidan <romecom2210@gmail.com>
2021-10-26 17:09:30 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use('/public', express.static(path.join(__dirname, 'static')));
+app.use(bodyParser.urlencoded({extended: false}))
+// app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'static', 'index.html'))
+});
+
+app.post('/', (req, res)=>{
+    console.log(req.body);
+
+    res.json({success: true});
+})
+
+
+app.listen(3000, () => {
+    console.log('Server start');
+});
