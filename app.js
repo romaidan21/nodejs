71be201ce33f80ad4c31709f41e1e09d3885e07b
Roman Maidan <romecom2210@gmail.com>
2021-10-26 22:12:08 +0300

    done

--- app.js
+++ app.js
@@ -4,19 +4,25 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use('/public', express.static(path.join(__dirname, 'static')));
-app.use(bodyParser.urlencoded({extended: false}))
-// app.use(cors());
-app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static', 'index.html'))
+app.set('view engine', 'ejs')
+
+// app.use(bodyParser.urlencoded({extended: false}))
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'static', 'index.html'))
+// });
+app.get('/:userQuery', (req, res) => {
+    res.render('index', {data: {userQuery: req.params.userQuery}})
 });
 
-app.post('/', (req, res)=>{
-    console.log(req.body);
+// app.post('/', (req, res)=>{
+//     console.log(req.body);
 
-    res.json({success: true});
-})
+//     res.json({success: true});
+// })
 
 
 app.listen(3000, () => {
